94bb82bdf282503f1de41a5d4074944e3bfd1922
Stephen.Chiu <itbuddy@163.com>
2018-11-28 09:54:05 +0800

    added mvn mybatis-spring added @MapperScan annotation

--- src/main/java/com/lx3/learning/LearningApplication.java
+++ src/main/java/com/lx3/learning/LearningApplication.java
@@ -1,12 +1,13 @@
 package com.lx3.learning;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 /**
  * @author mic
  */
 @SpringBootApplication
+@MapperScan("com.lx3.learning.dao")
 public class LearningApplication {
 
 	public static void main(String[] args) {
